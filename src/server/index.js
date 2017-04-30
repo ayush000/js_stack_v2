45299764ace5c10fde4a8a0356cabe2fd2c72186
Ayush Sachdeva <ayush@socialfrontier.com>
2017-05-01 02:23:13 +0530

    Added bootstrap and socket

--- src/server/index.js
+++ src/server/index.js
@@ -2,12 +2,18 @@
 
 import compression from 'compression'
 import express from 'express'
+import { createServer } from 'http'
+import socketIO from 'socket.io'
 
 import routing from './routing'
 import { WEB_PORT, STATIC_PATH } from '../shared/config'
 import { isProd } from '../shared/util'
+import setUpSocket from './socket'
 
 const app = express()
+const server = createServer(app)
+const io = socketIO(server)
+setUpSocket(io)
 
 app.use(compression())
 app.use(STATIC_PATH, express.static('dist'))
@@ -15,7 +21,7 @@ app.use(STATIC_PATH, express.static('public'))
 
 routing(app)
 
-app.listen(WEB_PORT, () => {
+server.listen(WEB_PORT, undefined, undefined, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' :
     '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`)
--- src/shared/component/page/hello.jsx
+++ src/shared/component/page/hello.jsx
@@ -1,14 +1,29 @@
 // @flow
 
 import React from 'react'
+import Helmet from 'react-helmet'
 
-import HelloButton from '../../container/hello-button'
 import Message from '../../container/message'
+import HelloButton from '../../container/hello-button'
+
+const title = 'Hello Page'
 
 const HelloPage = () =>
-  <div>
-    <Message />
-    <HelloButton />
+  <div className="container mt-4">
+    <Helmet
+      title={title}
+      meta={[
+        { name: 'description', content: 'A page to say hello' },
+        { property: 'og:title', content: title },
+      ]}
+    />
+    <div className="row">
+      <div className="col-12">
+        <h1>{title}</h1>
+        <Message />
+        <HelloButton />
+      </div>
+    </div>
   </div>
 
 export default HelloPage
